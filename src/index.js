ee105adde12934165bcf9ce25be16ad4568e1d9a
marwa-hodeib <marwahodeib31@gmail.com>
2021-03-02 15:35:47 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('canvas');
+
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
---
+++ src/indexSpec.js
@@ -0,0 +1,5 @@
+describe('test',() => {
+  it('give number 1', () => {
+    expect(1).toBe(1)
+  })
+})
